45c2fe40128f09ecb6f3762ef447c0695720c040
bgwiazda <01167304@pw.edu.pl>
2021-10-15 04:55:27 +0200

    Moment when i got all my work for appending week

---
+++ cal.py
@@ -0,0 +1,19 @@
+import icalendar as ical
+import pytz
+from datetime import datetime
+
+cal = ical.Calendar()
+cal.add('prodid', '-//My calendar product//mxm.dk//')
+cal.add('version', '2.0')
+
+def createEvent(location, dateStart, dateEnd):
+  event = ical.Event()
+  event.add('summary', 'Cinemacity Work')
+  event.add('dtstart', dateStart)
+  event.add('dtend', dateEnd)
+  event['location'] = ical.vText(location)
+  cal.add_component(event)
+
+def saveCal():
+  with open("cal.ics", 'wb') as f:
+    f.write(cal.to_ical())
