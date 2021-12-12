806a94c8349c88cb23723ae73f0d21dcc212bd72
bgwiazda <01167304@pw.edu.pl>
2021-12-12 21:35:10 +0100

    Added succesfull message

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os, update, time
 while True:
 	try:
 		update.update()
+		os.system("echo \"Succesful synch" + time.strftime("%H:%M:%S, %d.%m.%Y") +  "\" >> kadro.log")
 	except Exception as e:
 		os.system("echo \" " + str(e) + "\" >> kadro.log")
 		os.system("curl -X POST -d \"{\\\"text\\\": \\\"\\\"}\" -H \"Content-Type: application/json\" https://api.pushcut.io/LD6ePZS6z3PNXGXqHHgFU/notifications/KadroError")
