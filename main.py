d751bd394a09d42c388c6de0d13673d6f016046b
bgwiazda <01167304@pw.edu.pl>
2021-12-12 21:30:26 +0100

    Added print message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ while True:
 	try:
 		update.update()
 	except Exception as e:
+		os.system("echo \" " + str(e) + "\" >> kadro.log")
 		os.system("curl -X POST -d \"{\\\"text\\\": \\\"\\\"}\" -H \"Content-Type: application/json\" https://api.pushcut.io/LD6ePZS6z3PNXGXqHHgFU/notifications/KadroError")
 	finally:
 		time.sleep(3600)	
