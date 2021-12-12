4f2d362bd30b29aa06edb46c7fa3f2992629ec19
bgwiazda <01167304@pw.edu.pl>
2021-12-12 20:16:43 +0100

    removed getting token

--- gAcces.py
+++ gAcces.py
@@ -1,26 +1,14 @@
-from __future__ import print_function
 import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def getService():
   creds = None
-  if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', SCOPES)
-      creds = flow.run_local_server(port=0)
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
-
+  if not os.path.exists('token.json'):
+    raise Exception("No token")
+  creds = Credentials.from_authorized_user_file('token.json', SCOPES)
   return build('calendar', 'v3', credentials=creds)
 
 def getList(service) -> list:
